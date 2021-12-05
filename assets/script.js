e85e024c410e0c958d5637acb417e2e538177768
Omar Valdivia Samudio <omarvalsam@gmail.com>
2021-12-04 22:12:40 -0700

    inital Apex code

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+// retrieve user input
+
+
+// use userinput in api call
+// send out api call
+// retrieve data recieved from api call
+// post result into html page to display stats of players
+
+
+var settingsApex = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://apex-legends.p.rapidapi.com/stats/Legalleta/X1",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "apex-legends.p.rapidapi.com",
+		"x-rapidapi-key": "f9bc6f03c1mshb2c57ff601fa1adp19194fjsne941b06e9692"
+	}
+};
+
+// bttn function to look up code
+$(document).on('click', '#searchForUs', function(){ 
+  alert("button is clicked");
+  
+  // when button is clicked this will call on settingsApex
+  $.ajax(settingsApex).done(function (response) {
+    console.log(response);
+    // Name of the player
+    console.log(response.global.name);
+    // player level
+    console.log(response.global.level);
+    // Players rank
+    console.log(response.global.rank.rankName);
+    // KD 
+    console.log(response.total.kd.value);
+    // Total Kills
+    console.log(response.total.kills.value);
+  });
+});
