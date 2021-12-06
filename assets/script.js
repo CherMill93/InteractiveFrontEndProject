9b65eefb4fac78a8c3c072fa168c5cdfdcac485c
Omar Valdivia Samudio <omarvalsam@gmail.com>
2021-12-05 21:02:43 -0700

    final inital code

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,57 @@
 // retrieve user input
+var userNameEl = document.querySelector("#playerName")
+var userConsoleEl = document.querySelector("#userConsole")
 
+console.log(userNameEl);
 
 // use userinput in api call
 // send out api call
-// retrieve data recieved from api call
-// post result into html page to display stats of players
-
-
-var settingsApex = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://apex-legends.p.rapidapi.com/stats/Legalleta/X1",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "apex-legends.p.rapidapi.com",
-		"x-rapidapi-key": "f9bc6f03c1mshb2c57ff601fa1adp19194fjsne941b06e9692"
-	}
-};
-
 // bttn function to look up code
-$(document).on('click', '#searchForUs', function(){ 
+// $(document).on('click', '#searchForUs', function () { **was not needed keeping incase**
+$("#searchForUs").click(function () {
   alert("button is clicked");
-  
+  var gamerTag = $(userNameEl);
+  //need to get value of this gamer console (aka X1,PS4,PC)
+  var userConsole = $(userConsoleEl);
+  console.log(userConsole)
+  var gamerUrl = "https://apex-legends.p.rapidapi.com/stats/legalleta/x1";
+
   // when button is clicked this will call on settingsApex
-  $.ajax(settingsApex).done(function (response) {
-    console.log(response);
-    // Name of the player
-    console.log(response.global.name);
-    $("#name").text(response.global.name);
+  // $.ajax(settingsApex).done(function (response) {
+  $.ajax({
+    // var settingsApex = {
+    async: true,
+    crossDomain: true,
+    url:gamerUrl,
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "apex-legends.p.rapidapi.com",
+      "x-rapidapi-key": "f9bc6f03c1mshb2c57ff601fa1adp19194fjsne941b06e9692"
+    },
+      success: function (response) {
+      // retrieve data recieved from api call
+      console.log(response);
+      var playerLevel = response.global.level;
+      var playerKd = response.total.kd.value;
+      var playerTotalKills = response.total.kills.value;
 
-    // player level
-    console.log(response.global.level);
-    $("#ourLevel").append(response.global.level);
+      // Name of the player
+      console.log(response.global.name);
+      // player level
+      console.log(response.global.level);
+      // Players rank
+      console.log(response.global.rank.rankName);
+      // KD 
+      console.log(response.total.kd.value);
+      // Total Kills
+      console.log(response.total.kills.value);
 
-    // Players rank
-    console.log(response.global.rank.rankName);
-     $("#compRank").text(response.global.rank.rankName);
-
-    // KD 
-    console.log(response.total.kd.value);
-    $("#kd").append(response.total.kd.value);
-    
-    // Total Kills
-    console.log(response.total.kills.value);
-    $("#totalKills").append(response.total.kills.value);
-  });
-});
-
-$("#statsForUs").ready(function() {
-  $("#test1").click(function(){
-    $("p").text("hello");
-  });
+      // post result into html page to display stats of players
+      $("#name").text(response.global.name);
+      $("#ourLevel").text(playerLevel);
+      $("#compRank").text(response.global.rank.rankName);
+      $("#kd").html(playerKd);
+      $("#totalKills").html(playerTotalKills);
+      }
+})
 });
